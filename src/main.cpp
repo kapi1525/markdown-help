9029cca0a1f4a05958a2a8ba2e2c284d72c1d626
kapigames <kapigamesdev@gmail.com>
2022-02-21 18:32:43 +0100

    custom output and temp paths

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,15 @@
 void markdown_help::start() {
     std::cin.get();
     proj.load_project("mdhelp.json");
+
+    if(arguments.has("o")) {
+        proj.output_path = std::filesystem::absolute(arguments.get("o"));
+    }
+    
+    if(arguments.has("t")) {
+        proj.temp_path = std::filesystem::absolute(arguments.get("t"));
+    }
+
     proj.generate_chm();
 }
 
